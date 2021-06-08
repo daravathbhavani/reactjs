361bb284442d3b05cf660a2f4a2aa25eccf030e4
daravathbhavani <85394703+daravathbhavani@users.noreply.github.com>
2021-06-09 01:42:49 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Note () {
+
+return (
+
+<div className="note">
+
+<h1>Javascript and React.js camp review</h1>
+
+<p>
+  i Daravath Bhavani of kasturba gandhi degree and pg college for women 
+  attended the javascript and react js bootcamp for the 
+  for the time and it was the great experience being part of such great team. 
+
+</p>
+
+</div>
+);
+
+}
